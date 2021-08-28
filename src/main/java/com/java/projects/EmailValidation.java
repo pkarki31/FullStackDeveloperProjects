77bd5a133062a0e7330fdc26b54351b0e0c2cf54
pkarki10 <karki_pankaj@optum.com>
2021-08-28 23:07:18 +0530

    Email Validation Project Code

--- src/main/java/com/java/projects/EmailValidation.java
+++ src/main/java/com/java/projects/EmailValidation.java
@@ -1,4 +1,58 @@
 package com.java.projects;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class EmailValidation {
+
+    static int sizeArray = 0;
+    static String [] arr;
+    static String find=null;
+
+
+    public static String validateEmailId(String input){
+
+        for(int i=0;i<arr.length;i++){
+
+            if(arr[i].contains(input)){
+
+                return arr[i];
+            }
+
+        }
+
+        return "Not found";
+    }
+
+
+    public static void main (String [] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("How many emailId you want to insert : ");
+
+        sizeArray = sc.nextInt();
+
+        arr = new String[sizeArray];
+
+        Scanner sc1 = new Scanner(System.in);
+
+        for(int i=0;i<sizeArray;i++){
+
+            System.out.println("Enter email ID of employee : ");
+
+            arr[i]=sc1.nextLine();
+
+        }
+
+        Scanner sc2 = new Scanner(System.in);
+
+        System.out.println("Enter email id you want to find :");
+
+        find = sc2.next();
+
+        System.out.println("Employee email id : "+validateEmailId(find));
+
+
+    }
 }
